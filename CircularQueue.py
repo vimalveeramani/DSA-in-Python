1ec6547f81e85e3f270bb9a45c90e5f3cb1b5d7f
codecustard <109494546+vimalveeramani@users.noreply.github.com>
2023-07-19 09:19:05 +0530

    Update CircularQueue.py

--- CircularQueue.py
+++ CircularQueue.py
@@ -58,3 +58,108 @@ obj.dequeue()
 print("After removing an element from the queue")
 obj.printCQueue()
 
+
+'''
+Sure, I'd be happy to explain the code to you line by line!
+
+This code implements a circular queue data structure in Python. Here's what each line does:
+
+python
+Copy
+class MyCircularQueue():
+Defines a class called MyCircularQueue that will contain the methods and attributes of the circular queue.
+python
+Copy
+    def __init__(self, k):
+        self.k = k
+        self.queue = [None] * k
+        self.head = self.tail = -1
+Defines an initialization method for the MyCircularQueue class that takes a parameter k (the maximum size of the queue).
+Initializes the k attribute to the input k.
+Initializes a list called queue with k None values.
+Initializes the head and tail pointers to -1 (meaning the queue is empty).
+python
+Copy
+    def enqueue(self, data):
+        if ((self.tail + 1) % self.k == self.head):
+            print("The circular queue is full\n")
+Defines a method called enqueue that takes a parameter data (the data to be inserted into the queue).
+Checks if the queue is full (i.e., if tail + 1 modulo k equals head). If it is full, prints a message saying so.
+python
+Copy
+        elif (self.head == -1):
+            self.head = 0
+            self.tail = 0
+            self.queue[self.tail] = data
+If the queue is not full and the head pointer is at -1 (meaning the queue is empty), sets the head and tail pointers to 0 and inserts the data into the queue list at the tail position.
+python
+Copy
+        else:
+            self.tail = (self.tail + 1) % self.k
+            self.queue[self.tail] = data
+If the queue is not full and the head pointer is not at -1, increments the tail pointer by 1 (using modulo k to wrap around to the beginning if the end of the list is reached) and inserts the data into the queue list at the new tail position.
+python
+Copy
+    def dequeue(self):
+        if (self.head == -1):
+            print("The circular queue is empty\n")
+Defines a method called dequeue that removes the element from the front of the queue.
+Checks if the queue is empty (i.e., if head is at -1). If it is empty, prints a message saying so.
+python
+Copy
+        elif (self.head == self.tail):
+            temp = self.queue[self.head]
+            self.head = -1
+            self.tail = -1
+            return temp
+If the queue is not empty and head equals tail (meaning there is only one element in the queue), sets head and tail to -1 (meaning the queue is empty), saves the value of the element being removed to temp, and returns temp.
+python
+Copy
+        else:
+            temp = self.queue[self.head]
+            self.head = (self.head + 1) % self.k
+            return temp
+If the queue is not empty and head does not equal tail, saves the value of the element being removed to temp, increments the head pointer by 1 (using modulo k to wrap around to the beginning if the end of the list is reached), and returns temp.
+python
+Copy
+    def printCQueue(self):
+        if(self.head == -1):
+            print("No element in the circular queue")
+Defines a method called printCQueue that prints the elements in the queue.
+Checks if the queue is empty (i.e., if head is at -1). If it is empty, prints a message saying so.
+python
+Copy
+        elif (self.tail >= self.head):
+            for i in range(self.head, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
+If the queue is not empty and tail is greater than or equal to head, iterates through the queue list from head to tail (including both endpoints) and prints each element.
+python
+Copy
+        else:
+            for i in range(self.head, self.k):
+                print(self.queue[i], end=" ")
+            for i in range(0, self.tail + 1):
+                print(self.queue[i], end=" ")
+            print()
+If the queue is not empty and tail is less than head, iterates through the queue list from head to the end of the list, then from the beginning of thelist to tail (including both endpoints), and prints each element.
+python
+Copy
+obj = MyCircularQueue(5)
+obj.enqueue(1)
+obj.enqueue(2)
+obj.enqueue(3)
+obj.enqueue(4)
+obj.enqueue(5)
+print("Initial queue")
+obj.printCQueue()
+
+obj.dequeue()
+print("After removing an element from the queue")
+obj.printCQueue()
+Creates an instance of the MyCircularQueue class with a maximum size of 5.
+Inserts the values 1, 2, 3, 4, and 5 into the queue using the enqueue method.
+Prints the initial queue using the printCQueue method.
+Removes an element from the queue using the dequeue method.
+Prints the updated queue after element removal using the printCQueue method.
+'''
